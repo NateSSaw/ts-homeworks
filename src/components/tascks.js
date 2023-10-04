bc63f184bb1a163bbee1b87b79e8dcf6d3f46457
Nataliia Semenova <natasemenova1806@gmail.com>
2023-10-04 20:28:11 +0300

    done 3 tascks

---
+++ src/components/tascks.js
@@ -0,0 +1,6 @@
+// var age = 50;
+// var name = 'Max';
+// var toggle = true;
+// var empty = null;
+// var notInitialize;
+// var callback = function (a) { return 100 + a; };
---
+++ src/components/tascks.ts
@@ -0,0 +1,15 @@
+// let age: number = 50;
+// let name1: string = 'Max';
+// let toggle: boolean = true;
+// let empty: null = null;
+// let notInitialize: any;
+// let callback: (a: number) =>number =(a: number) => { return 100 + a };
+
+// let anything: any = -20;
+// anything = 'Text';
+// anything = {};
+
+// let some:unknown | any;
+// some = 'Text';
+// let str: string;
+// str = some as string;;
